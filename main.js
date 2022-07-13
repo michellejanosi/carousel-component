58ed6b72498428e13e3e162bf15aed5d2c1b0225
michellejanosi <michelle.janosi@gmail.com>
2022-07-13 06:24:13 -0400

    Add move to next slide function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const slides = document.getElementsByClassName('carousel-item');
 let slidePosition = 0;
-const totalSlides = slides.length;
+let totalSlides = slides.length;
 const carouselBtnNext = document.getElementById('carousel-button-next');
 const carouselBtnPrev = document.getElementById('carousel-button-prev');
 
@@ -27,5 +27,13 @@ function moveToNextSlide() {
 }
 
 function moveToPrevSlide() {
-  console.log('move to previous slide');
+  hideAllSlides();
+
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  slides[slidePosition].classList.add('carousel-item-visible');
 }
